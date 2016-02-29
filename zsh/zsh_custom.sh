196c809fc83be292ee4fafd2da311f667f73f9a9
Thiago Souza 'Roshi <thiagoroshi@keemail.me>
2016-02-29 13:28:10 -0300

    Update and rename zsh_custom to zsh_custom.sh

--- zsh/zsh_custom.sh
+++ zsh/zsh_custom.sh
@@ -2,23 +2,28 @@
 # Personal Customizations
 # @thiagoroshi
 ##
- export EDITOR="vim"
+
+# not view username in PS1 while 
+# logged in default user
+ DEFAULT_USER="thiago"
+
+# neovim > vim
+ export EDITOR="nvim"
+
+# command history control
  export HISTSIZE=1000;
  export HISTFILESIZE=$HISTSIZE;
  export HISTCONTROL=ignoredups;
  export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help";
-
- DEFAULT_USER="thiago"
-# Validando necessidade
-# eval `dircolors ~/.solarized/dircolors.ansi-dark
-
-# O que deixei para fazer?
+ 
+# Remeber me active works
  task
+
 # bora ver o que roda embaixo do capô?
 # screenfetch
-##
-# Estilizando o PowerLevel9K theme
-##
+
+# Customizing PowerLevel9K theme
+
 #POWERLEVEL9K_MODE='compatible'  
 # modes=(awesome-fontconfig, awesome-patched, compatible, flat)
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
