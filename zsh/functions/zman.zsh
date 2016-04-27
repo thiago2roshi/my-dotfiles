e1827d3bece39519e2711d7ed75a78942c0dfc79
Thiago Souza 'Roshi <thiagoroshi@keemail.me>
2016-04-27 13:21:24 -0300

    Create zman.zsh

---
+++ zsh/functions/zman.zsh
@@ -0,0 +1,7 @@
+# The manpage zshall contains everything
+# @author: Christian Neukirchen
+# http://chneukirchen.org/blog/archive/2012/02/10-new-zsh-tricks-you-may-not-know.html
+
+zman() {
+  PAGER="less -g -s '+/^       "$1"'" man zshall
+}
