e4a01f1df5139dc2408b8b51afdd6d87d57d54c3
thiago2roshi <ads.thiagosouza@gmail.com>
2016-08-13 01:21:03 -0300

    added rofi and adjusting things

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 ##
+# validate network on
+sudo dhcpcd wlp0s18f2u4 
 # programas para rodar no pos-boot do sistema
 # subindo mpd
-killall mpd ;  mpd ~/.config/mpd/mpd.conf
+killall mpd ; mpd ~/.config/mpd/mpd.conf
 mpd play
 # subindo conky
 conky -d -c /home/thiago/.config/conky/conky.conf
