f145a2b88abd8e877ad2139ab33f30d34dbdc420
thiago2roshi <ads.thiagosouza@gmail.com>
2018-01-29 23:30:01 -0200

    more ricing...

---
+++ scripts/wpa_notify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+case "$2" in
+    CONNECTED)
+        notify-send "WPA supplicant: connection established";
+        ;;
+    DISCONNECTED)
+        notify-send "WPA supplicant: connection lost";
+        ;;
+esac
+
