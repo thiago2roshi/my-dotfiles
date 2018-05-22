0b5fa974f5d2a914a0e576cfd9a9f7ad989884e3
thiago2roshi <ads.thiagosouza@gmail.com>
2018-05-22 15:13:53 -0300

    More color in my live

--- zsh/01_export_var.zsh
+++ zsh/01_export_var.zsh
@@ -25,13 +25,17 @@ export MANPATH="/usr/local/man:$MANPATH"
 source "${HOME}/.local/share/icons-in-terminal/icons_bash.sh"
 
 #  Less need some color to?
-# export LESS_TERMCAP_mb=$'\E[01;31m'
-# export LESS_TERMCAP_md=$'\E[01;31m'
-# export LESS_TERMCAP_me=$'\E[0m'
-# export LESS_TERMCAP_se=$'\E[0m'
-# export LESS_TERMCAP_so=$'\E[01;44;33m'
-# export LESS_TERMCAP_ue=$'\E[0m'
-# export LESS_TERMCAP_us=$'\E[01;32m'
+ export LESS_TERMCAP_mb=$'\E[01;31m'
+ export LESS_TERMCAP_md=$'\E[01;31m'
+ export LESS_TERMCAP_me=$'\E[0m'
+ export LESS_TERMCAP_se=$'\E[0m'
+ export LESS_TERMCAP_so=$'\E[01;44;33m'
+ export LESS_TERMCAP_ue=$'\E[0m'
+ export LESS_TERMCAP_us=$'\E[01;32m'
 
 # Zplugins
 TIPZ_TEXT=' 💡 alias: '
+
+# LS_COLORS
+source "${HOME}/.dotfiles/bash/LS_COLORS"
+
