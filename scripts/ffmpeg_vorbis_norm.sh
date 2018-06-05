d55df91fc4011e2a22ca32c0d7f9fefcc618ce66
thiago2roshi <ads.thiagosouza@gmail.com>
2018-06-04 22:51:59 -0300

    simple is better

--- scripts/ffmpeg_vorbis_norm.sh
+++ scripts/ffmpeg_vorbis_norm.sh
@@ -1,25 +1,16 @@
 #!/bin/zsh
+# Create file fast
+> /tmp/ffmpeg_convert_toOGG.log
 
-touch /tmp/ffmpeg_convert_toMP3.log
-touch /tmp/ffmpeg_convert_toOGG.log
+#find . -type f -name '*.mp3' -exec bash -c \
+#    'ffmpeg -i "$0" -y \
+#    -c:a libopus  \
+#    "${0/%mp3/ogg}" && \
+#    echo $0 >> /tmp/ffmpeg_converter_toOGG.log'  '{}' \;
 
-find . -type f -name '*.ogg' -exec bash -c 'ffmpeg -i "$0" -acodec libmp3lame -b:a 320k "${0/%ogg/mp3}" && echo $0 >> /tmp/ffmpeg_convert_toMP3.log ' '{}' \;
-
-#for FILE in /home/thiago/Desktop/MUSICAS-tes/**/*.ogg; do
-#    NFILE="${a[@]/%ogg/mp3}"
-#    ffmpeg -i $FILE -acodec libmp3lame -b:a 320k $NFILE
-#    echo $FILE >> /tmp/ffmpeg_convert_toMP3.log
-#done
-
-telegram-cli -W -e "msg Thiagoroshi 'ffmpeg to mp3 terminado 1/2'"
-
-find . -type f -name '*.mp3' -exec bash -c 'ffmpeg -i "$0" -y -acodec libvorbis -b:a 320k "${0/%mp3/ogg}" && echo $0 >> /tmp/ffmpeg_converter_toOGG.log && rm -rf "$0"' '{}' \;
-
-#for FILE in /home/thiago/Desktop/MUSICAS-tes/**/*.mp3; do
-#    NFILE="${a[@]/%ogg/mp3}"
-#    ffmpeg -i $FILE -acodec libvorbis -b:a 320k $NFILE
-#    echo $FILE >> /tmp/ffmpeg_convert_toOGG.log
-#    rm -rf $FILE 
-#done
-
-telegram-cli -W -e "msg Thiagoroshi 'ffmpeg to ogg terminado 2/2'"
+while read FILE; do
+    #NFILE="${a[@]/%mp3/opus}"
+    ffmpeg -i "$FILE" "${FILE/%ogg/opus}"
+    echo $FILE >> /tmp/ffmpeg_convert_toOGG.log
+    mv $FILE /tmp/$FILE
+done < ~/music/playlist/VORBIS\ QUE\ NAO\ FUNCIONAM.m3u
--- scripts/starter.sh
+++ scripts/starter.sh
@@ -3,19 +3,19 @@
 # programas para rodar no pos-boot do sistema
 
 # merge Xresources
-xrdb -merge ~/.Xresources &
-
-# sxhkd : keybinds 
-sxhkd &
+xrdb -merge ~/.Xresources
 
 # Lemonbar and notification things
 # use: output message in /tmp/beepop.fifo and /tmp/popup.fifo
-beebop & 
-popup-overlay &
+beepop & 
+#popup-overlay.sh &
+
+# sxhkd : keybinds 
+[[ -z $(pgrep -xU $UID sxhkd)       ]] && sxhkd &
 
 # subindo mpd
 [[ -z $(pgrep -xU $UID mpd )        ]] && mpd ~/.config/mpd/mpd.conf &
-sleep 2s && mpd play &
+sleep 2s && mpd play 
 #[[ -z $(pgrep -xU $UID mpdscribble) ]] && mpdscribble 
 #[[ -z $(pgrep -xU $UID mopidy) ]] && mopidy
 
@@ -24,8 +24,10 @@ sleep 2s && mpd play &
 
 #Up WiFi
 #netctl start enp0s18f2u4-RS_G && sleep 1s
-pkexec wifi.sh connect RS_G "1597rafael" && sleep 1s
-
+#pkexec wifi.sh connect RS_G "1597rafael" &
+#sleep 2s
 # subindo conky
-[[ -z $(pgrep -xU $UID conky)       ]] && conky \
-            -q -c $HOME/.dotfiles/config/conky/conky.conf &
+#[[ -z $(pgrep -xU $UID conky)       ]] && conky.sh &
+
+# wpa_cli notification
+wpa_cli -a ~/bin/wpa_notify.sh
