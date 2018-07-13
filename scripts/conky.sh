7fac165cc5ef83881809bc17a5563947ce74edc7
thiago2roshi <ads.thiagosouza@gmail.com>
2018-07-13 15:02:57 -0300

    organizing bin

--- scripts/conky.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# conky_starter() {
-#    for CONKY in $HOME/.config/conky/*.conky; do
-#        conky -q -c $CONKY &
-#    done
-#}
-# conky_starter || echo 'fail to execute conky files'
-sleep 3s && (conky -q -c $HOME/.dotfiles/config/conky/conky.conf || echo 'CONKY: Fail to Execute conky'> /tmp/beebop.fifo)
